7733b773842342c8c93f584e68e13dd55cc3b9e9
kevy60 <kevin.vorsmann@voco.ee>
2024-02-14 14:31:09 +0200

    Getted article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -16,7 +16,23 @@ const getAllArtticles = (req, res) => {
     })
 }
 
+const getArticleBySlug = (req, res) => {
+    Article.findOne({
+        where: {
+            slug : req.params.slug
+        }
+    })
+    .then(article => {
+        console.log(article)
+        return res.status(200).json({ article });
+    })
+    .catch (error => {
+    return res.status(500).send(error.message);
+    })
+};
+
 
 module.exports = {
-    getAllArtticles
-}
+    getAllArtticles,
+    getArticleBySlug
+};
--- index.js
+++ index.js
@@ -21,6 +21,7 @@ sequelize
 
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
+app.use('/article', articleRouter)
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to sequelize application."});
