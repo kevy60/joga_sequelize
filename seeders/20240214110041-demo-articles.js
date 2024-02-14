e7f50fadbe7e51e61ab12ee38a776523f9e6db9e
kevy60 <kevin.vorsmann@voco.ee>
2024-02-14 13:16:08 +0200

    Created article table and added table data

---
+++ seeders/20240214110041-demo-articles.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { Sequelize } = require("sequelize");
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Introduction to Ashtanga',
+        slug: 'Introductionb-to-ashtanga',
+        image: 'ashtanga.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipscing elit.</p>',
+        published: '2020-01-08 15:02:30',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Morning vinyasa flow routine',
+        slug: 'morning-vinyasa-flow-routine',
+        image: 'morning.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipscing elit.</p>',
+        published: '2020-04-14 15:02:41',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+      queryInterface.bulkInsert('Articles', [{
+        name: 'Secrets of a yoga teacher',
+        slug: 'secrets-of-a-yoga-teacher',
+        image: 'yoga-teacher.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipscing elit.</p>',
+        published: '2060-05-28 15:02:55',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]),
+    ])
+  },
+  down: (queueInterface, Sequelize) => {
+    return queueInterface.bulkDelete('Articles', null, {});
+  }
+};
+
